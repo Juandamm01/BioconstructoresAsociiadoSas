ab061e73e70b13d7e4d2f7ed7b8a71edb9c10321
Santiago Suescun <santiagosuescun@Santiagos-MacBook-Air.local>
2026-03-19 12:08:35 -0500

    fix:- Adjusted Image properties for responsive design.

--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -10,6 +10,7 @@
 
   import React, { useRef, useState } from "react";
   import Link from "next/link";
+  import Image from "next/image";
 
 
   interface NavbarProps {
@@ -205,6 +206,7 @@
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
+            onClick={onClose}
             className={cn(
               "absolute inset-x-0 top-16 z-50 flex w-full flex-col items-start justify-start gap-4 rounded-lg bg-white px-4 py-8 shadow-[0_0_24px_rgba(34,42,53,0.06),0_1px_1px_rgba(0,0,0,0.05),0_0_0_1px_rgba(34,42,53,0.04),0_0_4px_rgba(34,42,53,0.08),0_16px_68px_rgba(47,48,55,0.05),0_1px_0_rgba(255,255,255,0.1)_inset] dark:bg-neutral-950",
               className,
@@ -237,10 +239,13 @@
         href="#"
         className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
       >
-        <img
+        <Image
           src="/images/bcas-logo.png"
           alt="Bioconstructores Asociados SAS"
-          className="h-16"
+          width={64}
+          height={64}
+          className="h-16 w-auto"
+          priority={false}
         />
       </Link>
     );
