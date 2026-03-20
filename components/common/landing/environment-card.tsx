2e8a3e4c855574817c0d48c1a9a85cf72fe14e9b
Juandamm <juandamm90@gmail.com>
2026-03-20 13:36:43 -0500

    feat: introduce new Policy and About Us sections to the landing page, including interactive policy cards, environment cards, and a map component, all enhanced with GSAP animations.

--- components/common/landing/environment-card.tsx
+++ components/common/landing/environment-card.tsx
@@ -13,7 +13,7 @@ export function EnvironmentCard({
 }: EnvironmentCardProps) {
   return (
     <div
-      className={`relative z-30 lg:w-[50vw] w-96 lg:h-[70vh] md:w-[90vw] md:h-[50vh] h-80 flex-none ${rotation} overflow-hidden`}
+      className={`relative z-30 lg:w-[50vw] w-96 lg:h-[70vh] md:w-[90vw] md:h-[50vh] h-80 flex-none ${rotation} overflow-hidden rounded-[2.5rem] md:rounded-[2vw] border-[6px] md:border-[0.5vw] border-blue-950 shadow-2xl`}
     >
       {/* Overlay oscuro */}
       <div className="absolute inset-0 z-20 bg-linear-to-t from-black/50 via-black/10 to-transparent pointer-events-none" />
@@ -25,7 +25,7 @@ export function EnvironmentCard({
         width={384}
         height={400}
         priority
-        className="absolute inset-0 w-full h-full object-cover rounded-[2vw] border-[.5vw] border-background"
+        className="absolute inset-0 w-full h-full object-cover"
       />
     </div>
   );
