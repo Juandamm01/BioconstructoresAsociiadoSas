a12c94558b7ffca0ae5b0857f5d36d239fe2442f
Juandamm <juandamm90@gmail.com>
2026-03-27 22:15:44 -0500

    feat: create features landing page with modular component structure

---
+++ app/features/page.tsx
@@ -0,0 +1,27 @@
+import {
+  Services,
+  AboutUs,
+  Hero,
+  Navbar,
+  Plans,
+  Map,
+  Footer,
+} from "@/components/landing";
+import { Policy } from "@/components/landing/policy";
+
+export default function Home() {
+  return (
+    <div className="overflow-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <AboutUs />
+        <Services />
+        <Plans />
+        <Map />
+        <Policy />
+      </main>
+      <Footer />
+    </div>
+  );
+}
